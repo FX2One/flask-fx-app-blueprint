d23049c1e09058964780624168c5369e3e6a8ec0
FX2One <dfoxik@gmail.com>
2022-03-10 12:11:23 +0100

    layout pattern upgrade

--- app/account/views.py
+++ app/account/views.py
@@ -2,7 +2,10 @@ from flask import Blueprint, render_template
 
 account = Blueprint('account', __name__, template_folder='templates')
 
-@account.route('/home')
-def home():
-    return render_template('account/home.html')
+@account.route('/register')
+def register_page():
+    return render_template('account/register.html')
 
+@account.route('/login')
+def login_page():
+    return render_template('account/login.html')
